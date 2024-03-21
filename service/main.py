d9a0cfdce31777c1c657409d919080707fa1d579
Ryan <roche189@umn.edu>
2024-03-21 16:01:44 -0500

    updated version

--- service/main.py
+++ service/main.py
@@ -1,37 +1,69 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+
+if not load_dotenv(dotenv_path="/docker-entrypoint.d/.env"):
+    raise Exception("Could not load .env file")
+
+URI = "bolt://db:7687"
+AUTH = ("neo4j", os.getenv("NEO4J_PASSWORD"))
+
+driver = GraphDatabase.driver(URI, auth=AUTH)
+driver.verify_connectivity()
 
 app = FastAPI(docs_url="/docs")
 
 
 class BuildingEntryModel(BaseModel):
-    area: str
     name: str
-    thumb: str
+    thumbnail: str
+    navID: str
 
 
 @app.get("/areas")
+# TODO: Cache areas query after first run since it won't be changing
 async def get_areas():
-    areas = ["TestBuildings"]
-    return areas
+    return driver.execute_query(
+        'MATCH(n) UNWIND labels(n) AS label WITH DISTINCT label WHERE label '
+                                                  '<> "Building" RETURN label'
+    ).records[0]
 
 
 @app.get("/buildings/{area}")
-async def get_buildings_by_area(area):
-    # TODO query neo4j database for buildings with area tag
-    if area == "TestBuildings":
-        return ["Test 1", "Test 2", "Test 3"]
-    else:
-        return ["Walter Library", "Keller Hall", "Tate Hall"]
+async def get_buildings_by_area(area, response_model=BuildingEntryModel):
+    records =  driver.execute_query(
+        f"MATCH (n:{str(area)}) RETURN n",
+    ).records
+
+    return [record[0] for record in records]
 
 
-@app.get("/destinations/{building}")
-async def get_destinations_for_building(building: str):
-    # TODO query neo4j database and return an array of building names
-    return {"message": f"Destinations for {building}"}
+@app.get("/destinations/{building_id}")
+async def get_destinations_for_building(building_id: str):
+    records = driver.execute_query(
+        """
+        MATCH (startNode {navID: $inputNodeID}), (reachableNode:BuildingKey)
+        WHERE (startNode)-[*]-(reachableNode)
+        RETURN reachableNode
+        """,
+        inputNodeID=building_id
+    ).records
+
+    return [record[0] for record in records]
 
 
 @app.get("/routes/{start}-{end}")
 async def get_route(start: str, end: str):
     # Query the neo4j database and return an array of node id strings
-    return {"message": f"Start node id is {start} and end node id is {end}"}
+    records = driver.execute_query(
+        """
+        MATCH (startNode {navID: $start}), (endNode {navID: $end}), path = shortestPath((startNode)-[*]-(endNode))
+        RETURN nodes(path)
+        """,
+        start=start,
+        end=end
+    ).records
+
+    return records[0][0]
