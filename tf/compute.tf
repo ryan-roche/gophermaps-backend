b605e6f4cbac6adfccb67ee36a3706127972edb2
Ryan <roche189@umn.edu>
2025-01-21 16:56:11 -0600

    finished compute

--- tf/compute.tf
+++ tf/compute.tf
@@ -1,1 +1,23 @@
-#* Create a launch template for the backend EC2 instances
+#* Define the EC2 instance
+resource "aws_instance" "app_server" {
+  ami                         = "ami-0030d50a3ce637c3a"
+  instance_type               = "t4g.small"
+  subnet_id                   = aws_subnet.public_a.id
+  key_name                    = "GopherMapsSSH"
+  vpc_security_group_ids      = [aws_security_group.backend.id]
+  associate_public_ip_address = true  # Needs an IPv4 address for CodeDeploy
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+
+  iam_instance_profile = "CodeDeploy-EC2-Instance_profile"
+  user_data            = file("ec2-setup.sh")
+
+  tags = {
+    Name        = "gophermaps-backend"
+    Environment = "production"
+    Application = "gophermaps" # For CodeDeploy to identify the instance
+  }
+}
---
+++ tf/ec2-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install git, python, and pip
+dnf update -y
+dnf install -y git
+dnf install -y python3.11
+dnf install -y python3.11-pip
+
+# Install poetry using pip
+pip3.11 install poetry==1.8.3
+
+# Create directory structure
+mkdir -p /var/gophermaps/app
+
+# Define the service for the FastAPI app that'll be used in the CodeDeploy scripts
+cat << 'EOF' > /etc/systemd/system/gophermaps.service
+[Unit]
+Description=GopherMaps FastAPI Application
+After=network.target
+
+[Service]
+WorkingDirectory=/var/gophermaps/app
+Environment="PATH=/var/gophermaps/app/venv/bin"
+ExecStart=/var/gophermaps/app/venv/bin/fastapi run --app main:app --host 0.0.0.0 --port 8000
+
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
