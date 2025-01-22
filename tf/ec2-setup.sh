7857694f70f71cd667529dd1070986864f435548
Ryan <roche189@umn.edu>
2025-01-22 00:24:03 -0600

    fix service definition

--- tf/ec2-setup.sh
+++ tf/ec2-setup.sh
@@ -20,8 +20,7 @@ After=network.target
 
 [Service]
 WorkingDirectory=/var/gophermaps/app
-Environment="PATH=/var/gophermaps/app/venv/bin"
-ExecStart=/var/gophermaps/app/venv/bin/fastapi run --app main:app --host 0.0.0.0 --port 8000
+ExecStart=/usr/local/bin/poetry run fastapi run --app main:app --host 0.0.0.0 --port 8000
 
 Restart=always
 RestartSec=3
